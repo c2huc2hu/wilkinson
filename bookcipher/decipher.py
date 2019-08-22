46be342db97721ef452921f52e96f34be87d027d
c2huc2hu <c2huc2hu@gmail.com>
2019-08-22 01:29:01 -0700

    start working on basic noisy channel model

--- bookcipher/decipher.py
+++ bookcipher/decipher.py
@@ -1,14 +1,124 @@
-# functions for handling ciphertext
+# functions for deciphering text
+
+import re
 
 def basic_substitute(ciphertext, wordbank):
     '''
     replace strings in ciphertext with known decryptions from wordbank
 
     ciphertext: string
-    wordbank: dict ()
+    wordbank: instance of Wordbank
     '''
 
-    return ciphertext
+    token_list = tokenize_ciphertext(ciphertext)
+    apply_wordbank(token_list, wordbank)
+
+    return ' '.join(token_list)
+
+def tokenize_ciphertext(ciphertext):
+    '''
+    return a token_list
+    '''
+    return list(map(Token, re.split(r'\s', ciphertext)))
+
+def apply_wordbank(token_list, wordbank):
+    for token in token_list:
+        if token in wordbank:
+            token.plaintext = wordbank[token]
+
+def substitute_guess(ciphertext, wordbank):
+    '''
+    interpolate guesses
+    '''
+
+    tokenize_ciphertext(ciphertext)
+
+def load_dictionary(filename):
+    result = {} # word -> index
+    with open(filename) as f:
+        result = {line: idx for idx, line in enumerate(f)}
+    return result
+
+# noisy channel model
+def lm(token_list):
+    '''get one-hot probabilities over the output space'''
+
+    return np.arange(10000)
+
+def prior(wordbank, token):
+    '''
+    unigram log-probability of each token given its approximate location. returns an array of the same shape as lm
+    model P(decoding|encoding) with a beta distribution (because it's normalized 0 to 1)
+    '''
+
+    left, right = wordbank.query_range(token)
+
+    # beta distribution centred about token_idx
+    token_idx = # estimate position of token in a modern dictionary
 
 
-def
+
+    mean = int(token)
+    variance = 0.1
+    n = mean * (1 - mean) / variance
+
+    dist_params = {
+        a: mean * n,
+        b: (1 - mean) * n,
+        loc:int(left), # shift and scale to 0 mean, 1 variance
+        scale:int(right)-int(left)
+    }
+
+    if dist_params['a'] < 1 or dist_params['b'] < 1:
+        raise ValueError('a, b must be > 1 to have a unimodal distribution')
+
+    result = np.zeros(10000,)
+    for word in vocab:
+        if left.plaintext < word < right.plaintext:
+            scipy.stats.beta.logcdf(token_idx + 1, **dist_params) - scipy.stats.beta.logcdf(token_idx, **dist_params)
+
+import collections
+import copy
+Candidate = collections.namedtuple('Candidate', ['tokens', 'lm_logprob', 'prior_logprob'])
+
+def decode(token_list, beam_size=16):
+    '''beam search decoding'''
+
+    beam = [Candidate(tokens=[], lm_logprob=0, prior_logprob=0)]
+
+    for token in token_list:
+        candidates = []
+
+        for beam_idx, candidate in enumerate(beam):
+            if token.ciphertype is None or token.plaintext != '<unk>':
+                # probably a hard-coded string. doesn't affect our model's probability
+                candidate.tokens.append(token)
+
+            elif token.ciphertype == 'dict':
+                # prior is a beta distribution over the bounds established by the wordbank table
+
+                likelihood = lm(candidate.tokens)
+
+                bounds =
+
+                # get K best extensions to the current sequence and add them to the beam
+                K = (beam_size - beam_idx)
+
+                best_extensions = np.argsort(likelihood) # shape: (N,)
+
+                for ext_idx in best_extensions:
+                    new_token = copy.copy(token)
+                    new_token.plaintext = idx_to_word[ext_idx]
+                    candidate.tokens.append(new_token)
+                    candidate.lm_logprob = np.log(likelihood[ext_idx])
+
+            elif token.ciphertype == 'table':
+                # prior is a uniform distribution over all english words
+                # only need the $beam_size most likely candidates
+
+                pass
+
+        # evaluate LM on everything
+        # prune all but the $beam_size most likely states
+
+    return most likely decoding
--- bookcipher/wordbank.py
+++ bookcipher/wordbank.py
@@ -1,27 +1,84 @@
 # functions for handling wordbanks
 
-def load_wordbank(filename, source_filter=None):
-    '''
-    Load a wordbank file into a dictionary {number: }
+import re
+from avltree import TokenAVL
 
-    filename: path to wordbank file. data should be tab separated in 3 columns: (location, source, word)
-    source_filter: list of sources to include. useful if multiple ciphers were used. defaults to including everything
-    '''
-    # TODO: Future improvement: one more letters are wordbanked, it would be useful to automatically
-    # partition letters according to which dictionary they came from. should be pretty straightforward
+class Token():
+    dict_re = r'(?P<page>\d+)\.\(?P<row>[\d+\])(?P<col>[=-])'
+    table_re = r'(?P<row>\[\d+\])\^'
 
-    wordbank = {}
+    def __init__(self, s, plaintext='<unk>'):
+        self.raw = s.strip()
+        self.plaintext = plaintext
+        self.ciphertype = None # which type of cipher this uses. one of: None (unparsed), dict or table
 
-    with open(filename) as f:
-        next(f) # skip header line
+        # parse
+        m = re.match(dict_re, self.raw):
+        if m:
+            self.ciphertype = 'dict'
+            self.page = int(m.group('page'))
+            self.row = int(m.group('row'))
+            self.col = {'-': 0, '=': 1}[m.group('col')] # - is col 1, = is col 2
+        m = re.match(table_re, self.raw)
+        if m:
+            self.ciphertype = 'table'
+            self.row = m.group('row')
 
-        for line in f:
-            try:
-                location, source, word = line.strip().split('\t')
-            except ValueError:
-                pass # blank line; skip
+    def __sub__(self, other):
+        '''get the number of words between this and another token'''
 
-            if source_filter is None or source in source_filter:
-                wordbank[location] = word
+        if self.ciphertype != 'dict' or other.ciphertype != 'dict': # can't compare
+            return None
+        return int(self) - int(other)
 
-    return wordbank
+    def __int__(self):
+        return (2 * 29 * (self.page - 1) # TODO: change 29 when using other wordbanks
+                + 29 * self.col
+                + (self.row - 1))
+
+    def __hash__(self):
+        return hash(self.raw) # need to be able to compare tokens with plaintext set and those without. this makes that equivalence true
+
+    def __str__(self):
+        return self.plaintext
+
+class Wordbank():
+    def __init__(self):
+        self._dict = {} # mapping Token -> word
+        self._tree = TokenAVL() # avl tree storing tokens. used too keep track of ranges
+
+    def load(self, filename):
+        with open(filename) as f:
+            next(f) # skip header line
+
+            for line in f:
+                try:
+                    location, source, word = line.strip().split('\t')
+                except ValueError:
+                    pass # blank line; skip
+
+                if source_filter is None or source in source_filter:
+                    t = Token(location, word)
+                    self._dict[t] = word
+                    self._tree.insert(t)
+
+    # imaginary words that would go at the beginning and end of a dictionary
+    left_dummy = Token('1.[1]-', '')
+    right_dummy = Token('780.[1]-', 'zzzzzzzz')     # the last attested word is 44,312 = yourself. it will only affect words after "yourself" in the dictionary, so doesn't really matter
+
+    def query_range(self, query_token):
+        '''return the approximate position in the range'''
+        if query_token.ciphertype is None or query_token.ciphertype == 'table':
+            return None
+
+        left, right = self._tree.find_neighbours(query_token)
+
+        # error handling for beginning or end of string
+        if left is None and right is None:
+            raise ValueError('no tokens have been inserted')
+        elif left is None:
+            left = left_dummy
+        elif right is None:
+            right = right_dummy
+
+        return left, right
