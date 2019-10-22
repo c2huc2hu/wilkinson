9a4270352238e155cac40fa5e24a7dcde546ec6b
Christopher Chu <chrischu@didiglobal.com>
2019-10-22 13:25:22 -0700

    fix bug so that the targets and logits are correctly aligned (verified with previous build), skip GPT when not necessoary

--- bookcipher/general_beamsearch.py
+++ bookcipher/general_beamsearch.py
@@ -149,7 +149,7 @@ class GPTLanguageModel(LanguageModel):
         # Can't run GPT on sentences with length 1. Make up a super-generic history
         if len(context) <= 1:
             # context = self.tokenizer.encode(self.tokenizer.unk_token) * (2 - len(context)) + context
-            context = self.tokenizer.encode('Yes.') + context
+            context = self.encode('Yes.') + context
 
         tokenized_input = [self.encode(word) for word in words]
 
@@ -159,33 +159,49 @@ class GPTLanguageModel(LanguageModel):
         for i, sent in enumerate(tokenized_input):
             np_input[i,:len(tokenized_input[i])] = tokenized_input[i]
 
-        batch_size = 128 # batch up compute so we don't run out of GPU memory
+        batch_size = 256 # batch up compute so we don't run out of GPU memory
         cross_entropies = np.zeros(np_input.shape[0]) # accumulate results in this
 
         with torch.no_grad():
             # Run context to get and cache past. This prevents runtime from growing as the context gets longer.
             # This could be saved from previous rounds, but that's probably unnecessary
-            past_input = torch.tensor([context], device=device)
-            past_logits, past = model(past_input)
+            past_input = torch.tensor([context], device=self.device)
+            past_logits, past = self.model(past_input)
+            
+            # Sort data by length
+            order = np.argsort(-word_lengths)
+                
+            for batch_start in range(0, np_input.shape[0], batch_size):
+                batch_indices = order[batch_start:batch_start+batch_size]
+                batch_lengths = word_lengths[batch_indices]
+                batch_inputs = np_input[batch_indices,:max(batch_lengths)]
+                
+                true_batch_size = len(batch_lengths) # the actual size of the batch, accounting for the final batch being smaller
 
-        for batch_start in range(0, np_input.shape[0], batch_size):
-            batch_lengths = word_lengths[batch_start:batch_start+batch_size]
-            batch_inputs = np_input[batch_start:batch_start+batch_size,:max(batch_lengths)]
+                # actually do GPT compute only if necessary. otherwise can just take it from final logits of previous step
+                if max(batch_lengths) > 1:
 
-            with torch.no_grad():
+                    # Evaluate on present state
+                    present_input = torch.tensor(batch_inputs, dtype=torch.long, device=self.device)
 
-                # Evaluate on present state
-                present_input = torch.tensor(batch_inputs, dtype=torch.long, device=device)
-                print('ps', present_input.shape, past[-1].shape)
+                    # Run the model with each new possibility (batched). We do a lot of unnecessary computation here
+                    # Every possibility is batched, even single word ones for which we know the answer
+                    past_ = torch.stack(past)
+                    past_ = past_.expand(-1,-1,true_batch_size,-1,-1,-1)
+                    present_logits, _present = self.model(present_input, past=past_)
+                    
 
-                # Run the model with each new possibility (batched). We do a lot of unnecessary computation here
-                # Every possibility is 
-                present_logits, _present = model(present_input, past=past[-1].expand(1,-1,batch_inputs.shape[0],-1,-1,-1))
+                    # Calculate loss for each possible completion. Can't use huggingface's summary because it summarizes over words, not sentences
+                    # Could cache the loss for the past, but I don't think that's the bottleneck
+                    logits = torch.cat((past_logits.expand(true_batch_size,-1,-1), present_logits), dim=1).transpose(1,2)[:,:,:-1]
+                    targets = torch.cat((past_input.expand(true_batch_size,-1), present_input), dim=1)[:,1:]
 
-                # Calculate loss for each possible completion. Can't use huggingface's summary because it summarizes over words, not sentences
-                # Could cache the loss for the past, but I don't think that's the bottleneck
-                logits = torch.cat((past_logits.expand(present_logits.shape[0],-1,-1), present_logits), dim=1).transpose(1,2)
-                targets = torch.cat((past_input.expand(present_input.shape[0],-1), present_input), dim=1)
+                # skip GPT call for words of length 1
+                else:
+                    logits = past_logits.expand(true_batch_size,-1,-1).transpose(1,2)
+                    present_input = torch.tensor(batch_inputs, dtype=torch.long)
+                    targets = torch.cat((past_input.expand(true_batch_size,-1), present_input), dim=1)[:,1:]
+
                 loss_fcn = torch.nn.CrossEntropyLoss(ignore_index=-1, reduction='none')        
                 losses = loss_fcn(
                     logits,
@@ -194,11 +210,11 @@ class GPTLanguageModel(LanguageModel):
 
                 # compute mean loss
                 # possible optimization: move this onto GPU? 
-                # torch doesn't support newaxis broadcasting, and it requires an extra copy, so possibly no impact
-                mask = ~(len(context) + batch_lengths + 1 <= np.arange(1,len(context) + max(batch_lengths) + 1)[:,None]).T
-                
+                # torch doesn't support newaxis broadcasting, and it requires an extra copy, so probably no impact
+                mask = ~(len(context) + batch_lengths <= np.arange(1,len(context) + max(batch_lengths))[:,None]).T
                 np_losses = losses.detach().cpu().numpy()
-                cross_entropies[batch_start:batch_start+batch_size] = (mask * np_losses).sum(axis=1)
+                cross_entropies[batch_indices] = (mask * np_losses).sum(axis=1)
+
                 
         return [LMScore(tokens=tokens, score=lm_prob) for tokens, lm_prob in zip(tokenized_input, -cross_entropies)]
 
